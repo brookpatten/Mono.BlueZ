e6fc4c2fc95272abb577295fb554ff92bf858299
Brook Patten <brook.patten@gmail.com>
2016-01-12 21:49:19 -0500

    first pass at all dbus interfaces that I have on hand

--- Mono.BlueZ.DBus/Interfaces.cs
+++ Mono.BlueZ.DBus/Interfaces.cs
@@ -37,11 +37,107 @@ namespace Mono.BlueZ.DBus
 	[Interface("org.bluez.Adapter1")]
 	public interface Adapter1
 	{
+		void RemoveDevice(ObjectPath device);
+		void SetDiscoveryFilter (IDictionary<string,object> properties);
+		void StartDiscovery();
+		void StopDiscovery();
+
+		IList<string> UUIDs{ get; }
+		bool Discoverable{get;set;}
+		bool Discovering{get;}
+		bool Pairable{get;set;}
+		bool Powered{get;set;}
+		string Address{ get; }
+		string Alias{get;set;}
+		string Modalias{ get; }
+		string Name{get;}
+		uint Class{ get; }
+		uint DiscoverableTimeout{get;set;}
+		uint PairableTimeout{ get; set; }
 	}
 
 	[Interface("org.bluez.CyclingSpeedManager1")]
 	public interface CyclingSpeedManager1
 	{
+		void RegisterWatcher (ObjectPath agent);
+		void UnregisterWatcher(ObjectPath agent);
+	}
+
+	[Interface("org.bluez.HeartRateManager1")]
+	public interface HeartRateManager1
+	{
+		void RegisterWatcher (ObjectPath agent);
+		void UnregisterWatcher(ObjectPath agent);
+	}
+
+	[Interface("org.bluez.Media1")]
+	public interface Media1
+	{
+		void RegisterEndpoint (ObjectPath endpoint,IDictionary<string,object> properties);
+		void RegisterPlayer (ObjectPath player,IDictionary<string,object> properties);
+		void UnregisterEndpoint(ObjectPath endpoint);
+		void UnregisterPlayer(ObjectPath player);
+	}
+
+	[Interface("org.bluez.NetworkServer1")]
+	public interface NetworkServer1
+	{
+		void Register (string uuid,string bridge);
+		void Unregister(string uuid);
+	}
+
+	[Interface("org.bluez.ThermometerManager1")]
+	public interface ThermometerManager1
+	{
+		void DisableIntermediateMeasurement (ObjectPath endpoint);
+		void EnableIntermediateMeasurement (ObjectPath player);
+		void RegisterWatcher(ObjectPath endpoint);
+		void UnregisterWatcher(ObjectPath player);
+	}
+
+	[Interface("org.bluez.Device1")]
+	public interface Device1
+	{
+		void CancelPairing();
+		void Connect();
+		void ConnectProfile(string UUID);
+		void Disconnect();
+		void DisconnectProfile(string UUID);
+		void Pair ();
+
+		IList<string> UUIDs{get;}
+		bool Blocked{get;set;}
+		bool Connected{ get; }
+		bool LegacyPairing{ get; }
+		bool Paired{get;}
+		bool Trusted{get;set;}
+		short RSSI{ get; }
+		ObjectPath Adapter{ get; }
+		string Address{get;}
+		string Alias{get;}
+		string Icon{ get; }
+		string Modalias{get;}
+		string Name{get;}
+		ushort Appearance{get;}
+		uint Class{get;}
+	}
+
+	[Interface("org.bluez.MediaControl1")]
+	public interface MediaControl1
+	{
+		void FastForward();
+		void Next();
+		void Pause();
+		void Play();
+		void Previous();
+		void Rewind();
+		void Stop();
+		void VolumeDown();
+		void VolumeUp();
+
+		bool Connected{get;}
 	}
 }
+
+
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Mono.BlueZ
-Wrappers &amp; Classes for interfacing with bluetooth via BlueZ from Mono
+Wrappers &amp; Classes for interfacing with bluetooth via BlueZ 5 from Mono via DBus
+
+Uses NDesk.DBus for dbus calls
+NDesk.DBus Documentation: http://www.ndesk.org/DBus_Documentation
+
+If you're poking around in DBus I highly recommend a tool such as d-feet
+
+BlueZ 5 Documentation: http://git.kernel.org/cgit/bluetooth/bluez.git/tree/doc
+
+Information on converting from BlueZ <5 to 5+
+http://www.bluez.org/bluez-5-api-introduction-and-porting-guide/
