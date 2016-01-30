3fbb9ccb4936457c8cc24efb593d599fb1c09ffa
Brook Patten <brook.patten@gmail.com>
2016-01-30 15:12:31 -0500

    added standalone dbus class to manage thread

---
+++ Mono.BlueZ.DBus/DBusConnection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using DBus;
+using org.freedesktop.DBus;
+using System.IO;
+
+namespace Mono.BlueZ.DBus
+{
+	public class DBusConnection:IDisposable
+	{
+		private Bus _system;
+		public Exception _startupException{ get; private set; }
+		private ManualResetEvent _started = new ManualResetEvent(false);
+		private Thread _dbusLoop;
+		private bool _run=false;
+		private bool _isStarted=false;
+
+		public DBusConnection ()
+		{
+			Startup ();
+		}
+
+		public Bus System
+		{
+			get
+			{
+				if (_isStarted) 
+				{
+					return _system;
+				} 
+				else 
+				{
+					throw new InvalidOperationException ("Not connected to DBus");
+				}
+			}
+		}
+
+		private void Startup()
+		{
+			// Run a message loop for DBus on a new thread.
+			_run = true;
+			_dbusLoop = new Thread(DBusLoop);
+			_dbusLoop.IsBackground = true;
+			_dbusLoop.Start();
+			_started.WaitOne(60 * 1000);
+			_started.Close();
+			if (_startupException != null) 
+			{
+				throw _startupException;
+			}
+			else 
+			{
+				_isStarted = true;
+			}
+		}
+
+		private void DBusLoop()
+		{
+			try 
+			{
+				_system=Bus.System;
+			} 
+			catch (Exception ex) 
+			{
+				_startupException = ex;
+				return;
+			} 
+			finally 
+			{
+				_started.Set();
+			}
+
+			while (_run) 
+			{
+				_system.Iterate();
+			}
+		}
+
+		private void Shutdown()
+		{
+			_run = false;
+			try
+			{
+				_dbusLoop.Join(1000);
+			}
+			catch 
+			{
+				try 
+				{
+					_dbusLoop.Abort ();
+				} 
+				catch 
+				{
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_isStarted) {
+				Shutdown ();
+			}
+		}
+	}
+}
+
